2f38b56dd8c5020bbdcdfd3566c036be8dc31a29
morlakdzuu <xerandom@mail.ru>
2021-05-15 16:51:24 +0300

    addSign fixed

--- controller/signController.js
+++ controller/signController.js
@@ -22,25 +22,19 @@ async function addSign(req, res) {
     let userId = jwt.decode(req.headers.authorization).id;
     let lat = req.body.lat;
     let lon = req.body.lon;
-    let address = req.body.address;
     let name = req.body.name;
-    let filedata = req.file;
-    if(!filedata) {
-        res.status(500);
-        res.json({error: "Exception while working with file"});
-    }
-    let photo = __dirname.replace("controller", "uploads/") + filedata.filename;
-    let sign = new Sign(0, lat, lon, name, userId, photo, address);
+    let address = req.body.address;
     try {
+        let sign = new Sign(0, lat, lon, name, userId, address, 0);
         let signId = await signRepository.addSign(sign);
         await confirmedSignRepository.confirmSignById(signId);
+        sign.signId = signId;
+        res.json({message: sign});
     } catch (err) {
         logger.log(err.message);
-        res.json({message: err.message});
-        return;
+        res.status(500);
+        res.json({message: 'error'});
     }
-
-    res.json({message: 'success'});
 }
 
 async function confirmSign(req, res) {
