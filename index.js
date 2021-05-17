38321ec0e3e9f193db771558b22d386b885fb029
morlakdzuu <xerandom@mail.ru>
2021-05-18 01:13:11 +0300

    addSign fixed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ global.connections = []
 
 const port = 8080;
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ limit: '50mb' }));
 
 app.get('/', (req, res) => {
     console.log(__dirname + '/index.html');
