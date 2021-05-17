9bf431cc824aabf914e7b3e0b00ac3069f77b503
morlakdzuu <xerandom@mail.ru>
2021-05-18 00:24:38 +0300

    addSign fixed

--- controller/neuronController.js
+++ controller/neuronController.js
@@ -31,7 +31,7 @@ async function getPhoto(req, res) {
 async function sendPhotoInfo(req, res) {
     console.log(req.headers.id);
     req.on('data', function (data) {
-        console.log("data " + data);
+        console.log("data " + data.labels);
     });
     req.on('files', function (data) {
         console.log("files " + data.labels);
