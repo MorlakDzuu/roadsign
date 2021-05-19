4c235aaf48bee4e373e16dee0f1deb2a2304e074
morlakdzuu <xerandom@mail.ru>
2021-05-19 21:16:17 +0300

    addSign fixed

--- service/roadSnapService.js
+++ service/roadSnapService.js
@@ -4,7 +4,7 @@ const GOOGLE_API_KEY = "AIzaSyDN7fDyGLsp9XwsZWId2Y7EOHF_G5GGM2c";
 
 async function makeGoogleApiRequest(lat, lon) {
     try {
-        request.get({url:'https://roads.googleapis.com/v1/snapToRoads?path'
+        request.get({url:'https://roads.googleapis.com/v1/snapToRoads?path='
                 + lat + ',' + lon + '&key=' + GOOGLE_API_KEY}, function optionalCallback(err, httpResponse, body) {
             if (err) {
                 return console.log('upload failed:', err);
