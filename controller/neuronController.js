3d1a52d32d27f722fb10132c4b5aec83397c3c4a
morlakdzuu <xerandom@mail.ru>
2021-05-20 20:59:59 +0300

    addSign fixed

--- controller/neuronController.js
+++ controller/neuronController.js
@@ -61,6 +61,7 @@ async function sendPhotoInfo(req, res) {
             })
         });
         let sign = await signRepository.getSignByUuid(req.headers.id);
+        await signRepository.deleteSignFromQueueBySignId(sign.id);
         for (let i = 0; i < labels.length; i++) {
             let signModel = new Sign(sign.id, sign.lat, sign.lon, labels[i], sign.user_id, uuid, sign.address, sign.direction);
             let id = await signRepository.addSign(signModel);
