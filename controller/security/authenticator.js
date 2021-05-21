0b994f001b13f6058f0741034ce2f1243730189e
morlakdzuu <xerandom@mail.ru>
2021-05-21 03:50:50 +0300

    admin

--- controller/security/authenticator.js
+++ controller/security/authenticator.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const config = require('../../config');
 const logger = require('../../service/logService');
-
+const userRepository = require('../../repository/userRepository');
 
 function apiAuthenticateJWT(req, res, next) {
     const token = req.headers.authorization;
@@ -17,18 +17,25 @@ function apiAuthenticateJWT(req, res, next) {
     });
 }
 
-function apiAuthenticateAdminJWT(req, res, next) {
+async function apiAuthenticateAdminJWT(req, res, next) {
     const token = req.headers.authorization;
-    jwt.verify(token, config.jwtApiAccessToken, (error, user) => {
-        if (error || jwt.decode(token).role == 'user') {
-            logger.log(error);
-            res.status(401);
-            res.json({message: "auth error"});
-            return;
-        }
-        req.user = user;
-        next();
-    });
+    try {
+        let user = await userRepository.getUserById(jwt.decode(token).id);
+        jwt.verify(token, config.jwtApiAccessToken, (error, user) => {
+            if (error || user.role == 'user') {
+                logger.log(error);
+                res.status(401);
+                res.json({message: "auth error"});
+                return;
+            }
+            req.user = user;
+            next();
+        });
+    } catch (err) {
+        logger.log(err);
+        res.status(401);
+        res.json({message: "auth error"});
+    }
 }
 
 module.exports = {
